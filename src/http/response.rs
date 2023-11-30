bf57817949ca699a1fe736fe6eb696b9fc0ba121
Richard Capraro <richard.capraro@gmail.com>
2023-11-30 20:36:04 +0100

    Initial commit

---
+++ src/http/response.rs
@@ -0,0 +1,31 @@
+use std::fmt::Display;
+use std::io::Write;
+use std::net::TcpStream;
+
+use crate::http::status_code::StatusCode;
+
+#[derive(Debug)]
+pub struct Response {
+    status_code: StatusCode,
+    body: Option<String>,
+}
+
+impl Response {
+    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+        Response { status_code, body }
+    }
+
+    pub fn send(&self, stream: &mut impl Write) -> std::io::Result<()> {
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\n\r\n{}",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            body
+        )
+    }
+}
---
+++ src/server.rs
@@ -0,0 +1,50 @@
+use std::io::Read;
+use std::net::TcpListener;
+
+use crate::http::{ParseError, Request, Response, StatusCode};
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
+
+pub(crate) struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub(crate) fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    pub(crate) fn run(self, mut handler: impl Handler) {
+        println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        loop {
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(req) => handler.handle_request(&req),
+                                Err(e) => handler.handle_bad_request(&e),
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to parse a request: {}", e);
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+        }
+    }
+}
